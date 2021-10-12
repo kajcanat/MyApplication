b2930b93483c7e74158e5c32e1b4427a299c0f75
Justin Canat <jc@bnkbl.com>
2021-10-12 10:27:56 +0200

    add gson module

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // todo some content
+        val gson = GsonBuilder().create()
     }
 }
