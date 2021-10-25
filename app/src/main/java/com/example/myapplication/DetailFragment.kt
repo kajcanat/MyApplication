cd385bcfa465acdb578d5c2467da8b90cdeb9669
Justin Canat <jc@bnkbl.com>
2021-10-25 14:40:58 +0200

    navigation to detail fragment

---
+++ app/src/main/java/com/example/myapplication/DetailFragment.kt
@@ -0,0 +1,82 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+        Log.i("Intents", "Action = $param1 $param2")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val param = this.param1
+        Log.i("Intents", "Action = $param")
+        val id  = param?.let { this.whenAssign(it) }
+        val planetImageView = view.findViewById<ImageView>(R.id.planetImage)
+        planetImageView.setImageResource(id as Int)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment DetailFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                DetailFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
+                }
+    }
+
+    private fun whenAssign(obj: Any): Any {
+        val result = when (obj) {
+            "earth" -> R.drawable.earth
+            "mars" -> R.drawable.mars
+            "jupiter" -> R.drawable.jupiter
+            "saturn" -> R.drawable.saturn
+            else -> 0
+        }
+        return result
+    }
+}
+
